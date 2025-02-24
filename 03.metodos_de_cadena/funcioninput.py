92a096b12fecf72b954c96409b61dbf301749e36
victoria324 <victoriaparrales213@gmail.com>
2025-02-24 17:26:18 -0600

    hola

---
+++ 03.metodos_de_cadena/funcioninput.py
@@ -0,0 +1,2 @@
+datos_personales= input("ingrese su nombre, epallido y edad: ")
+print(datos_personales)
---
+++ 03.metodos_de_cadena/lower.py
@@ -0,0 +1,5 @@
+nombre_1= "gretel  victoria parrales rodriguez"
+nombre_2= "MARIA JOSE RODRIGUEZ"
+
+print(nombre_1.upper())
+print(nombre_2.lower())
